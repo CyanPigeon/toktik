02f2fe83b73ec80d1b13709a445ada1802904c71
CAMB <cambdev@outlook.com>
2023-08-22 15:30:56 +0800

    refactor: 注释错误和调整传入参数

--- utils/db.go
+++ utils/db.go
@@ -10,9 +10,9 @@ type Database struct {
 }
 
 type IDBQuery interface {
-	ExecuteRawSql(string, map[string]interface{}, interface{}) error
+	ExecuteRawSql(string, map[string]interface{}, *interface{}) error
 	Query(interface{}, *interface{}) error
-	Insert(interface{}) error
+	Insert(*interface{}) error
 	GetDB() *gorm.DB
 }
 
@@ -53,11 +53,11 @@ func (db *Database) Query(comp interface{}, model *interface{}) error {
 //		})
 //
 // 插入成功将返回nil,否则返回error
-func (db *Database) Insert(data interface{}) error {
+func (db *Database) Insert(data *interface{}) error {
 	r := db.Db.Clauses(clause.Locking{
 		Strength: "ROW EXCLUSIVE",
 		Table:    clause.Table{Name: clause.CurrentTable},
-	}).Create(&data)
+	}).Create(data)
 	if r.Error != nil {
 		return r.Error
 	}
@@ -67,8 +67,8 @@ func (db *Database) Insert(data interface{}) error {
 // ExecuteRawSql 直接执行SQL语句,不建议用,不保证稳定性
 //
 // s为RawSql语句，m为用于替换RawSql中?部分的map，model为返回的模型
-func (db *Database) ExecuteRawSql(s string, m map[string]interface{}, model interface{}) error {
-	r := db.Db.Exec(s, m).Scan(&model)
+func (db *Database) ExecuteRawSql(s string, m map[string]interface{}, model *interface{}) error {
+	r := db.Db.Exec(s, m).Scan(model)
 	if r.Error != nil {
 		return r.Error
 	}
